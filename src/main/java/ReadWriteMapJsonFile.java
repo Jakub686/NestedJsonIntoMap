2f78f8b9fbce2238cbd855f81cf511ea15e802f8
Jakub <jakub.bchyla@gmail.com>
2022-05-29 11:18:21 +0200

    working independent read and write

--- src/main/java/ReadWriteMapJsonFile.java
+++ src/main/java/ReadWriteMapJsonFile.java
@@ -11,10 +11,10 @@ public class ReadWriteMapJsonFile {
     public static void main(String[] args) {
         try {
             //hashmap
-            HashMap<String, String> sampleMap = new HashMap<>();
-            sampleMap.put("1", "aaa");
-            sampleMap.put("3", "bbb");
-            sampleMap.put("2", "ccc");
+            HashMap<String, Object> sampleMap = new HashMap<>();
+            sampleMap.put("1", new Person("a",new Address("b","c")));
+            sampleMap.put("2", new Person("dddddddddddd",new Address("e","g")));
+            sampleMap.put("3","aaa");
 
             //serialization map into json
             ObjectMapper mapper = new ObjectMapper();
@@ -24,7 +24,6 @@ public class ReadWriteMapJsonFile {
             System.out.println("Json before save in a file" + jsonStr);
 
             //write json into file
-
             BufferedWriter write = new BufferedWriter(new FileWriter(PATH));
             write.write(jsonStr);
             write.close();
@@ -32,11 +31,11 @@ public class ReadWriteMapJsonFile {
             //-----------------------------------------------------
 
             //read map from json file
-            Map<String, String> map1 = mapper.readValue(new File(PATH), new TypeReference<Map<String, String>>() {});
+            Map<String, Object> map1 = mapper.readValue(new File(PATH), new TypeReference<Map<String, Object>>() {});
             //show map
-            System.out.println("Map from Jason: " + map1.get("1"));
-            System.out.println("Map from Jason: " + map1.get("2"));
-            System.out.println("Map from Jason: " + map1.get("3"));
+            System.out.println("Map from Jason file: " + map1.get("1"));
+            System.out.println("Map from Jason file: " + map1.get("2"));
+            System.out.println("Map from Jason file: " + map1.get("3"));
         }catch (IOException e){
         }
     }
